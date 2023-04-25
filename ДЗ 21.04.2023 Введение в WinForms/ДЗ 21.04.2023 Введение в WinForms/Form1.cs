9f1651a5098559dd435bb5e1063be67b1ff2bae6
Anna Boiko <annaboiko377@gmail.com>
2023-04-25 14:27:31 +0300

    !

--- ДЗ 21.04.2023 Введение в WinForms/ДЗ 21.04.2023 Введение в WinForms/Form1.cs
+++ ДЗ 21.04.2023 Введение в WinForms/ДЗ 21.04.2023 Введение в WinForms/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Вывести на экран свое (краткое!!!) резюме с помощью последова-
+//тельности MessageBox’ов (числом не менее трех). Причем на заголовке
+//последнего должно отобразиться среднее число символов на странице
+//(общее количество символов в резюме / количество MessageBox’ов).
 namespace ДЗ_21._04._2023_Введение_в_WinForms
 {
     public partial class Form1 : Form
@@ -15,6 +19,26 @@ namespace ДЗ_21._04._2023_Введение_в_WinForms
         public Form1()
         {
             InitializeComponent();
+            List<string> resume = new List<string>()
+            {
+                "Бойко Анна Борисовна",
+                "Дата рождения: 03.02.1995",
+                "Место рождения: Украина, г.Одесса",
+                "Образование: ЮНПУ имени К.Д. Ушинского",
+                "Профессия: учитель украинского языка и литературы"
+            };
+
+            int symb = 0;
+            int box = resume.Count;
+
+            foreach (string item in resume)
+            {
+                symb += item.Length;
+                
+            }
+            MessageBox.Show(resume.Count.ToString());
+            MessageBox.Show(symb.ToString());
+            MessageBox.Show("Hello", " Message window");
         }
     }
 }
