6bfb1b7feef21fcf457603a2060476cd2fc75ac2
Anna Boiko <annaboiko377@gmail.com>
2023-04-30 00:44:42 +0300

    !

--- ДЗ 28.04.2023 Списки/ДЗ 28.04.2023 Списки/Form1.cs
+++ ДЗ 28.04.2023 Списки/ДЗ 28.04.2023 Списки/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,36 @@ namespace ДЗ_28._04._2023_Списки
 {
     public partial class Form1 : Form
     {
+        List<Person> list = new List<Person>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var p1 = new Person("Алексей", "Петров", "alex_petrov94@gmail.com","+380938754265" );
+            var p2 = new Person("Алёна", "Степанова", "al97_stepanova@gmail.com", "+380689845366");
+            var p3 = new Person("Владимир", "Фёдоров", "fedorov-vova1988@gmail.com", "+380689845366");
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Person(string name,string surname,string email, string phone)
+        {
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Phone = phone;
+        }
+        public override string ToString()
+        {
+            return $"Имя: {Name}\nФамилия: {Surname}\n" +
+                $"E-mail: {Email}\nТелефон: {Phone}";
         }
     }
 }
