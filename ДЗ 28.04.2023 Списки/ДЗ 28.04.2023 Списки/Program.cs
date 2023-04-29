55631b76b3ae2913d91bbf8b68eee2b18fd13592
Anna Boiko <annaboiko377@gmail.com>
2023-04-30 00:04:36 +0300

    !

---
+++ ДЗ 28.04.2023 Списки/ДЗ 28.04.2023 Списки/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ДЗ_28._04._2023_Списки
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
