a8ed8b112e9decc076de4ad80ae1185d230488bb
Anna Boiko <annaboiko377@gmail.com>
2023-05-19 00:59:25 +0300

    !

---
+++ ДЗ 18.05.2023 Контейнеры/ДЗ 18.05.2023 Контейнеры/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ДЗ_18._05._2023_Контейнеры
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
