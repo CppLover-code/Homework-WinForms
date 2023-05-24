e6abb79ce00ae46fb8cc15c1429825492f06a4ad
Anna Boiko <annaboiko377@gmail.com>
2023-05-24 23:54:51 +0300

    !

--- ДЗ 19.05.2023 Элементы управления/ДЗ 19.05.2023 Элементы управления/MainWindow.xaml.cs
+++ ДЗ 19.05.2023 Элементы управления/ДЗ 19.05.2023 Элементы управления/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ДЗ_19._05._2023_Элементы_управления
 {
@@ -20,9 +23,36 @@ namespace ДЗ_19._05._2023_Элементы_управления
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer? timer = null;
+        int flag = 0;
         public MainWindow()
         {
             InitializeComponent();
+            timerStart();
         }
+        private void timerStart()  // запуск таймера
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+            timer.Start();
+        }
+        private void timerTick(object sender, EventArgs e)  // событие таймера
+        {
+            flag += 10;
+            if (flag % 30 == 0)
+            {
+                tblStrDT.Text = "Дата:";
+                tblDateTime.Text = DateTime.Now.ToLongDateString();
+            }
+            else
+            {
+                tblStrDT.Text = "Время:";
+                tblDateTime.Text = DateTime.Now.ToLongTimeString();
+            }
+            tblDayOfWeek.Text = CultureInfo.GetCultureInfo("ru-RU").DateTimeFormat.GetDayName(DateTime.Now.DayOfWeek).ToString();
+        }
+
+
     }
 }
