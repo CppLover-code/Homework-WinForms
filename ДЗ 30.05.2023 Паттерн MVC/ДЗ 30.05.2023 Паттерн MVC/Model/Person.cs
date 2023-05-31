a030f7de47060ba0ca21189c57016bf325dedbe7
Anna Boiko <annaboiko377@gmail.com>
2023-05-31 03:25:02 +0300

    !

---
+++ ДЗ 30.05.2023 Паттерн MVC/ДЗ 30.05.2023 Паттерн MVC/Model/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ_30._05._2023_Паттерн_MVC.Model
+{
+    class Controller
+    {
+    }
+}
