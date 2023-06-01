9ef7460ecfb986f867caee222f436fa96e4ed10a
Anna Boiko <annaboiko377@gmail.com>
2023-06-02 02:50:33 +0300

    !

--- Пр. 01.06.2023 Ресурсы, триггеры/Практика Ресурсы/obj/Debug/net7.0-windows/App.g.i.cs
+++ Пр. 01.06.2023 Ресурсы, триггеры/Практика Ресурсы/obj/Debug/net7.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace Практика_Ресурсы {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Практика Ресурсы;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Практика Ресурсы;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
