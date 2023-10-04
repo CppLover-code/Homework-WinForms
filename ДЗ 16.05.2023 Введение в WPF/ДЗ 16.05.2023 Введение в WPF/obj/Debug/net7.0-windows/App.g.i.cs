668055e678cd897b715a975aaba902c05971667f
Anna Boiko <94569983+CppLover-code@users.noreply.github.com>
2023-10-04 17:31:54 +0300

    !

--- ДЗ 16.05.2023 Введение в WPF/ДЗ 16.05.2023 Введение в WPF/obj/Debug/net7.0-windows/App.g.i.cs
+++ ДЗ 16.05.2023 Введение в WPF/ДЗ 16.05.2023 Введение в WPF/obj/Debug/net7.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace ДЗ_16._05._2023_Введение_в_WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace ДЗ_16._05._2023_Введение_в_WPF {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.11.0")]
         public static void Main() {
             ДЗ_16._05._2023_Введение_в_WPF.App app = new ДЗ_16._05._2023_Введение_в_WPF.App();
             app.InitializeComponent();
